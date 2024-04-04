99957c75d50ac5d8dc6abfb4e4399c859f509b02
Azamat <azambylbaev@gmail.com>
2024-04-04 23:09:57 +0300

    some changes

--- accounts/routes/profile/changeUserInfo.py
+++ accounts/routes/profile/changeUserInfo.py
@@ -19,3 +19,10 @@ class MyProfileAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=200)
         return Response(serializer.errors, status=400)
+
+    def post(self, request):
+        serializer = UserInfoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
